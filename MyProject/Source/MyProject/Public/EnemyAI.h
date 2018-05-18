e54e993e2a93e55902fa0139613951d58c930752
Seton9 <pissergah3@gmail.com>
2018-05-18 09:12:37 -0700

    Test

---
+++ MyProject/Source/MyProject/Public/EnemyAI.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "EnemyAI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API AEnemyAI : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
